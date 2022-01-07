193e8c8d08c614d7517bb90ddbaca22aae39103f
kvammee <elliott.kvamme@gmail.com>
2022-01-07 14:28:26 -0600

    Lifted hooks to App level

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { capitalizeFirstLetter } from "../../utils/helpers";
+import photo from '../../assets/small/commercial/0.jpg';
 
 function Gallery() {
+    const currentCategory = {
+        name: "commercial",
+        description: "Photos of grocery stores, food trucks, and other commercial projects"
+    };
     return (
         <section>
-            <h2>NAME</h2>
-            <p>DESCRIPTION</p>
+            <h1>{capitalizeFirstLetter(currentCategory.name)}</h1>
+            <p>{currentCategory.description}</p>
+            <div className='flex-row'>
+                <img className='img-thumbnail mx-1' src={photo} alt="Commercial example" />
+            </div>
         </section>
     );
 };
