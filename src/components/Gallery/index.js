a5ec90d5266d4d2e149a458e66b7dcd700666987
kvammee <elliott.kvamme@gmail.com>
2022-01-07 12:27:00 -0600

    Created gallery skeleton

---
+++ src/components/Gallery/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Gallery() {
+    return (
+        <section>
+            <h2>NAME</h2>
+            <p>DESCRIPTION</p>
+        </section>
+    );
+};
+
+export default Gallery;
