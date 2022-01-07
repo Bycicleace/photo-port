eff49944eca8e1e62d507ec315476674c26f106f
kvammee <elliott.kvamme@gmail.com>
2022-01-07 16:36:54 -0600

    Added modals

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 
-function Modal() {
+function Modal({currentPhoto}) {
+    const { name, category, description, index } = currentPhoto;
+
     return (
-        <div>
-            
+        <div className='modalBackdrop'>
+            <div className='modalContainer'>
+                <h3 className='modalTitle'>{name}</h3>
+                <img src={require(`../../assets/large/${category}/${index}.jpg`)} alt="current category" />
+                <p>
+                    {description}
+                </p>
+                <button type="button">
+                    Close this modal
+                </button>
+            </div>
         </div>
     );
 };
